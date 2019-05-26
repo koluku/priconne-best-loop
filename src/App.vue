c4b819736f9779dedc4dba538cc7bed43a6bce97
koluku <koluku@users.noreply.github.com>
2019-05-26 22:44:00 +0900

    検証用のビュー

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <ul>
+            <li v-for="area in areas" v-bind:key="area.name">
+                {{ area.name }}
+                <ul>
+                    <li v-for="drop in area.drops" v-bind:key="drop.id">
+                        {{ itemIDToItemName(drop.id) }}
+                    </li>
+                </ul>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Areas from "./components/areas.json";
+import Items from "./components/items.json";
 
 export default {
     name: "app",
-    components: {
-        HelloWorld
+    data: function() {
+        return {
+            areas: Areas.areas,
+            items: Items.items
+        };
+    },
+    methods: {
+        itemIDToItemName: function(itemID) {
+            for (let item of this.items) {
+                if (itemID === item.id) {
+                    return item.name;
+                }
+            }
+        }
     }
 };
 </script>
-
-<style lang="scss">
-#app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
